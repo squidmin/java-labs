ab490dd9932f0de96d697bc891bacc9e3a1d652b
squidmin <morse.james.r@gmail.com>
2023-01-23 14:45:21 -0800

    Binary literals: Minor edits

--- src/main/java/binaryliterals/BinaryLiterals.java
+++ src/main/java/binaryliterals/BinaryLiterals.java
@@ -27,8 +27,7 @@ public class BinaryLiterals {
             quotient /= 2;
         }
 
-        binaryNumber = binaryNumber.reverse();
-        return Integer.valueOf(binaryNumber.toString());
+        return Integer.valueOf(binaryNumber.reverse().toString());
     }
 
     // Addition of binary numbers
@@ -44,9 +43,7 @@ public class BinaryLiterals {
             firstNum = firstNum / 10;
             secondNum = secondNum / 10;
         }
-        if (carry != 0) {
-            output.append(carry);
-        }
+        if (carry != 0) output.append(carry);
         return Integer.valueOf(output.reverse().toString());
     }
 
@@ -55,11 +52,8 @@ public class BinaryLiterals {
         StringBuilder onesComplement = new StringBuilder();
         while (num > 0) {
             int lastDigit = num % 10;
-            if (lastDigit == 0) {
-                onesComplement.append(1);
-            } else {
-                onesComplement.append(0);
-            }
+            if (lastDigit == 0) onesComplement.append(1);
+            else onesComplement.append(0);
             num = num / 10;
         }
         return Integer.valueOf(onesComplement.reverse().toString());
@@ -75,45 +69,37 @@ public class BinaryLiterals {
             temp = (firstNum % 10 + onesComplement % 10 + carry) % 2;
             output.append(temp);
             carry = (firstNum % 10 + onesComplement % 10 + carry) / 2;
-
             firstNum = firstNum / 10;
             onesComplement = onesComplement / 10;
         }
         String additionOfFirstNumAndOnesComplement = output.reverse().toString();
-        if (carry == 1) {
-            return addBinaryNumber(Integer.valueOf(additionOfFirstNumAndOnesComplement), carry);
-        } else {
-            return getOnesComplement(Integer.valueOf(additionOfFirstNumAndOnesComplement));
-        }
+        if (carry == 1) return addBinaryNumber(Integer.valueOf(additionOfFirstNumAndOnesComplement), carry);
+        else return getOnesComplement(Integer.valueOf(additionOfFirstNumAndOnesComplement));
     }
 
     public static void main(String[] args) {
-        // Byte type Binary Literal
-        byte a1 = 0b011;
+        byte a1 = 0b011;  // Byte type Binary Literal
 
         // The b can be lower or upper case
         byte a2 = 0B101;
         System.out.println("Binary Literal in Byte----->");
         System.out.println("a1 = " + a1 + ", " + "a2 = " + a2);
 
-        // Short type Binary Literal
-        short b1 = 0b101;
+        short b1 = 0b101;  // Short type Binary Literal
 
         // The b can be lower or upper case
         short b2 = 0B111;
         System.out.println("Binary Literal in Short----->");
         System.out.println("b1 = " + b1 + ", " + "b2 = " + b2);
 
-        // Int type Binary literal
-        int c1 = 0b011;
+        int c1 = 0b011;  // Int type Binary literal
 
         // The b can be lower or upper case
         int c2 = 0B111;
         System.out.println("Binary Literal in Integer----->");
         System.out.println("c1 = " + c1 + ", " + "c2 = " + c2);
 
-        // Long type Binary literal
-        long d1 = 0b0000011111100011;
+        long d1 = 0b0000011111100011;  // Long type Binary literal
 
         // The b can be lower or upper case
         long d2 = 0B0000011111100001;
