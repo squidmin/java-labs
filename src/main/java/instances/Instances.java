e3c495eeeb4a61127ea8febaf6d3f45ef5451c80
squidmin <morse.james.r@gmail.com>
2023-01-26 14:36:33 -0800

    Fix typos

--- src/main/java/instances/Instances.java
+++ src/main/java/instances/Instances.java
@@ -5,7 +5,7 @@ public class Instances {
     // This instance variable is visible for any child class.
     public String field_1;
 
-    // Salary variable is visible in Employee class only.
+    // field_2 variable is visible in Instances class only.
     private double field_2;
 
     // The name variable is assigned in the constructor.
@@ -13,18 +13,18 @@ public class Instances {
         this.field_1 = field_1;
     }
 
-    // The salary variable is assigned a value.
+    // The field_2 variable is assigned a value.
     public void setField_2(double field_2) {
         this.field_2 = field_2;
     }
 
-    // This method prints the employee details.
+    // This method prints the instance field values.
     public void printFields() {
-        System.out.println("field_1  : " + field_1);
-        System.out.println("field_2 :" + field_2);
+        System.out.println("field_1: " + field_1);
+        System.out.println("field_2:" + field_2);
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Instances instance = new Instances("lol");
         instance.setField_2(1000);
         instance.printFields();
