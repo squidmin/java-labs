dbdd7fa9865cb2ddb760ce26f8c748bc366ceebb
squidmin <morse.james.r@gmail.com>
2023-02-05 18:52:44 -0800

    Edit: Instances: Singleton example (cont'd)

--- src/main/java/instances/EnumSingletonTest.java
+++ src/main/java/instances/EnumSingletonTest.java
@@ -2,6 +2,14 @@ package instances;
 
 public class EnumSingletonTest {
     public static void main(String[] args) {
-
+        EnumSingleton instance = EnumSingleton.INSTANCE.getInstance();
+        System.out.println(instance.hashCode());
+        /*
+         * This approach has serialization and thread-safety guaranteed by the enum implementation itself, which
+         *   ensures internally that only the single instance is available, correcting the thread-safety problems with
+         *   the class-based Singleton implementation.
+         */
+        instance = EnumSingleton.INSTANCE.getInstance();
+        System.out.println(instance.hashCode());  // Prints the same hash code.
     }
 }
