f4a3bf8577300c3530e864fe97a8ab33be6c396d
squidmin <morse.james.r@gmail.com>
2023-02-16 00:05:31 -0600

    Add test case for bounded type demo

--- src/main/java/generics/BoundedTypeDemo.java
+++ src/main/java/generics/BoundedTypeDemo.java
@@ -72,7 +72,8 @@ public class BoundedTypeDemo {
     public static void main(String[] args) {
         GeometricObject rectangle = new Rectangle(2, 2);
         GeometricObject circle = new Circle(2);
-        System.out.println("Same area? " + BoundedTypeDemo.<GeometricObject>equalArea(rectangle, circle));
+        System.out.println("Same area? " + BoundedTypeDemo.<GeometricObject>equalArea(rectangle, circle));  // false
+        System.out.println("Same area? " + BoundedTypeDemo.<GeometricObject>equalArea(rectangle, rectangle));  // false
     }
 
 }
