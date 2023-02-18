e93487a0619ea83e3ea732a316b1c587794c8de6
squidmin <morse.james.r@gmail.com>
2023-02-17 22:26:25 -0600

    Add: Exceptions: TryWithResources test cases

--- src/main/java/exceptions/TryWithResources.java
+++ src/main/java/exceptions/TryWithResources.java
@@ -1,35 +1,29 @@
 package exceptions;
 
-class ClassA implements AutoCloseable {
-    public ClassA() {
-        System.out.println("ClassA constructor called.");
-    }
-
-    public void method() throws Exception {
-        throw new Exception("ClassA method called.");
-    }
-
-    public void close() throws Exception {
-        System.out.println("ClassA close() method called.");
-        throw new Exception("Unable to complete close() invocation!");
-    }
-}
-
-class ClassB implements AutoCloseable {
-    public ClassB() {
-        System.out.println("ClassB constructor called.");
-    }
-
-    public void method() throws Exception {
-        throw new Exception("ClassB method called.");
-    }
-
-    public void close() throws Exception {
-        System.out.println("ClassB close() method called.");
-    }
-}
-
 public class TryWithResources {
+
+    static class ClassA implements AutoCloseable {
+        public ClassA() {
+            System.out.println("ClassA constructor called.");
+        }
+
+        public void method() throws Exception { throw new Exception("ClassA method called."); }
+
+        @Override
+        public void close() throws Exception {
+            System.out.println("ClassA close() method called.");
+            throw new Exception("Unable to complete close() invocation!");
+        }
+    }
+
+    static class ClassB implements AutoCloseable {
+        public ClassB() { System.out.println("ClassB constructor called."); }
+
+        public void method() throws Exception { throw new Exception("ClassB method called."); }
+
+        @Override
+        public void close() { System.out.println("ClassB close() method called."); }
+    }
 
     public static void main(String[] args) {
         try (ClassA classA = new ClassA(); ClassB classB = new ClassB()) {
---
+++ src/test/java/exceptions/TryWithResourcesTest.java
@@ -0,0 +1,24 @@
+package exceptions;
+
+import org.junit.Test;
+
+public class TryWithResourcesTest {
+
+    @Test(expected = Exception.class)
+    public void classA_close_whenInvoked_throwsException() throws Exception {
+        try (TryWithResources.ClassA instance = new TryWithResources.ClassA()) {}
+    }
+
+    @Test(expected = Exception.class)
+    public void classA_method_whenInvoked_throwsException() throws Exception {
+        try (TryWithResources.ClassA instance = new TryWithResources.ClassA()) {
+            instance.method();
+        }
+    }
+
+    @Test(expected = Exception.class)
+    public void classB_method_whenInvoked_throwsException() throws Exception {
+        try (TryWithResources.ClassB instance = new TryWithResources.ClassB()) { instance.method(); }
+    }
+
+}
