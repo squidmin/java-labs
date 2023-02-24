f9832f4abb879b9238b7ff0f74110164334a3587
squidmin <morse.james.r@gmail.com>
2023-02-24 15:57:51 -0600

    Refactor: Binary: Bitwise operators reference

--- src/main/java/binary/BitwiseOperators.java
+++ src/main/java/binary/BitwiseOperators.java
@@ -1,18 +1,32 @@
 package binary;
 
+import java.util.function.BiFunction;
+
 public class BitwiseOperators {
 
-    public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 9;
+    private static String formatBinary(int num) {
+        return String.format("%4s", Integer.toBinaryString(num)).replace(' ', '0');
+    }
 
-        System.out.println("Bitwise AND: " + (num1 & num2));
-        System.out.println("Bitwise OR: " + (num1 | num2));
-        System.out.println("Bitwise XOR: " + (num1 ^ num2));
-        System.out.println("Bitwise NOT: " + (~num1));
-        System.out.println("Bitwise Left Shift: " + (num1 << 2));
-        System.out.println("Bitwise Right Shift: " + (num1 >> 2));
-        System.out.println("Bitwise Unsigned Right Shift: " + (num1 >>> 2));
+    private static void echoTheOp(String operation, BiFunction<Integer, Integer, Integer> fn) {
+        int num1 = 5, num2 = 9;
+        System.out.println(formatBinary(num1));
+        System.out.println(formatBinary(num2));
+        System.out.println(operation + ":");
+        Integer fnResult = fn.apply(num1, num2);
+        System.out.println(formatBinary(fnResult));
+        System.out.println("Decimal: " + fnResult);
+        System.out.println("-".repeat(30));
+    }
+
+    public static void main(String[] args) {
+        echoTheOp("Bitwise AND", (a, b) -> (a & b));
+        echoTheOp("Bitwise OR", (a, b) -> (a | b));
+        echoTheOp("Bitwise XOR", (a, b) -> (a ^ b));
+        echoTheOp("Bitwise NOT", (a, b) -> (a ^ b));
+        echoTheOp("Bitwise Left Shift", (a, b) -> (a << b));
+        echoTheOp("Bitwise Right Shift", (a, b) -> (a >> b));
+        echoTheOp("Bitwise Unsigned Right Shift", (a, b) -> (a >>> b));
     }
 
 }
