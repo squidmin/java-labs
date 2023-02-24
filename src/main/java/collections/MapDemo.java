1ff59b60fb4e964eeee72e0df16160b3b6c7088b
squidmin <morse.james.r@gmail.com>
2023-02-24 16:42:05 -0600

    Edit: Collections: Map demo & test

--- src/main/java/collections/MapDemo.java
+++ src/main/java/collections/MapDemo.java
@@ -3,21 +3,21 @@ package collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MapUpgradesDemo {
+public class MapDemo {
 
     public static Map<String, Integer> withoutGetOrDefault() {
         Map<String, Integer> fruits = new HashMap<>();
-        fruits.put("apple", 20);
+        fruits.put("perfume", 20);
 
-        // We need to add 20 bananas in map.
-        // Below line will throw NullPointerException if banana.
-        // is already not present in the map.
+        /*
+         * Add 20 items to the map.
+         * Below line will throw NullPointerException if key doesn't exist in the map.
+         */
+        // fruits.put("genius", fruits.get("genius") + 20);
 
-        // fruits.put("banana", fruits.get("banana") + 20);
-
-        // This is the correct way to update map value before Java 8.
-        if (fruits.containsKey("banana")) fruits.put("banana", fruits.get("banana") + 20);
-        else fruits.put("banana", 20);
+        // This is the correct way to update a map value *before Java 8*.
+        if (fruits.containsKey("genius")) { fruits.put("genius", fruits.get("genius") + 20); }
+        else { fruits.put("genius", 20); }
 
         System.out.println(fruits);
 
@@ -26,18 +26,18 @@ public class MapUpgradesDemo {
 
     public static Map<String, Integer> withGetOrDefault() {
         Map<String, Integer> fruits = new HashMap<>();
-        fruits.put("apple", 20);
-        fruits.put("banana", fruits.getOrDefault("banana", 0) + 20);
+        fruits.put("perfume", 20);
+        fruits.put("genius", fruits.getOrDefault("genius", 0) + 20);
         System.out.println(fruits);
         return fruits;
     }
 
     public static Map<String, Integer> putIfAbsent() {
         Map<String , Integer> fruits = new HashMap<>();
-        fruits.put("apple", 20);
-        System.out.println(fruits.get("apple"));
-        fruits.putIfAbsent("apple", 30);
-        System.out.println(fruits.get("apple"));
+        fruits.put("perfume", 20);
+        System.out.println(fruits.get("perfume"));
+        fruits.putIfAbsent("perfume", 30);
+        System.out.println(fruits.get("perfume"));
         return fruits;
     }
 
--- src/test/java/collections/MapDemoTest.java
+++ src/test/java/collections/MapDemoTest.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MapUpgradesDemoTest {
+public class MapDemoTest {
 
     @Test
     public void getOrDefault_resultsAreEqual() {
-        Map<String, Integer> with = MapUpgradesDemo.withoutGetOrDefault();
-        Map<String, Integer> without = MapUpgradesDemo.withGetOrDefault();
+        Map<String, Integer> with = MapDemo.withoutGetOrDefault();
+        Map<String, Integer> without = MapDemo.withGetOrDefault();
         Assert.assertEquals(with, without);
     }
 
     @Test
     public void putIfAbsent_returnExpectedResult() {
         Map<String, Integer> expected = new HashMap<>();
-        expected.put("apple", 20);
-        Map<String, Integer> actual = MapUpgradesDemo.putIfAbsent();
+        expected.put("perfume", 20);
+        Map<String, Integer> actual = MapDemo.putIfAbsent();
         Assert.assertEquals(expected, actual);
     }
 
